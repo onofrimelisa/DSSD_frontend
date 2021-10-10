c097cb0adac4501610e9d6a9de54a094ce2b6c2c
Melisa Ailén Onofri <onofrimelisa@gmail.com>
2021-10-10 16:58:36 -0300

    add swal alerts

--- src/app/legales/estatuto-sociedades/estatuto-sociedades.component.ts
+++ src/app/legales/estatuto-sociedades/estatuto-sociedades.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PRIVATE_BACKEND_URL } from 'src/app/app-constants';
+import swal from 'sweetalert';
 
 export interface PeriodicElement {
   name: string;
@@ -29,9 +32,29 @@ export class EstatutoSociedadesComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'apoderado', 'symbol'];
   dataSource = ELEMENT_DATA;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  aprobarSociedad() {
+    this.http.post(PRIVATE_BACKEND_URL, "1")
+      .subscribe((data) => {
+        swal("Aprobar Sociedad", "La operación se realizó correctamente", "success");
+      }, (error) => {
+        swal("Aprobar Sociedad", "Ocurrió un problema", "error");
+      })
+
+  }
+
+  desaprobarSociedad() {
+    this.http.post(PRIVATE_BACKEND_URL, "1")
+      .subscribe((data) => {
+        swal("Desaprobar Sociedad", "La operación se realizó correctamente", "success");
+      }, (error) => {
+        swal("Desaprobar Sociedad", "Ocurrió un problema", "error");
+      })
+
   }
 
 }
