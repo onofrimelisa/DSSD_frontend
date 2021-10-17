49cf3a8e6b68d4188929a1964e0104e624483cb7
Melisa Ailén Onofri <onofrimelisa@gmail.com>
2021-10-17 17:25:01 -0300

    add sociedades for legales

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { MESA_ENTRADAS_ROLE, LEGALES_ROLE } from '../app-constants';
+import { MESA_ENTRADAS_ROLE, LEGALES_ROLE, APODERADO_ROLE } from '../app-constants';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -10,10 +10,7 @@ import { AuthService } from '../services/auth.service';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor(public auth: AuthService, private router: Router) {
-    console.log(this.auth.isUserLogged());
-
-  }
+  constructor(public auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -33,8 +30,12 @@ export class ToolbarComponent implements OnInit {
         this.router.navigate(["/legales/sociedades"])
         break;
 
-      default:
+      case APODERADO_ROLE:
         this.router.navigate(["/apoderado/sociedades"])
+        break;
+
+      default:
+        this.router.navigate(["/login"])
         break;
     }
   }
