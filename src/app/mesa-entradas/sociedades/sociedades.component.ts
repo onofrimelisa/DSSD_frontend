fa4abf96fbbe5a3eb896a04580b2cc2e585e4f7d
Melisa Ailén Onofri <onofrimelisa@gmail.com>
2021-10-17 17:08:59 -0300

    add sociedades for mesa_entradas

--- src/app/mesa-entradas/sociedades/sociedades.component.ts
+++ src/app/mesa-entradas/sociedades/sociedades.component.ts
@@ -1,27 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { PRIVATE_BACKEND_URL } from 'src/app/app-constants';
+import { MesaEntradasService } from 'src/app/services/mesa-entradas.service';
 import swal from 'sweetalert';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
 
 @Component({
   selector: 'app-sociedades',
@@ -29,10 +10,9 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./sociedades.component.css']
 })
 export class SociedadesComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'apoderado', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['nombre', 'fechaCreacion', 'apoderado', 'estado', 'operaciones'];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public mesaEntradas: MesaEntradasService) { }
 
   ngOnInit(): void {
   }
---
+++ src/app/services/mesa-entradas.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { PRIVATE_BACKEND_URL } from '../app-constants';
+import { SociedadAnonima } from '../interfaces';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MesaEntradasService {
+  sociedades: SociedadAnonima[] = []
+  loading: boolean = true
+
+  constructor(private http: HttpClient, private auth: AuthService) {
+    this.getSociedades()
+  }
+
+  getSociedades() {
+    this.http.get(PRIVATE_BACKEND_URL + "/sociedades?username=" + this.auth.getLocalStorage("username"))
+      .subscribe((result: any) => {
+        this.loading = false
+        this.sociedades = result
+        console.log(this.sociedades);
+      }, (err) => {
+        console.log(err);
+      })
+  }
+}
